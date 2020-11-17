d7a5e8b19f58d7a7b831cca4fcc662c734d8ebe8
I. Reyes <iker.reyes@irbbarcelona.org>
2020-11-17 10:05:53 +0100

    Added long description to setup and other minor components

--- setup.py
+++ setup.py
@@ -7,11 +7,19 @@ directory = path.dirname(path.abspath(__file__))
 with open(path.join(directory, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
+with open(path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     name='hotspots_framework',
     version=__version__,
     description='BBGLab tool',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
-    install_requires=required
+    install_requires=required,
+    url="https://bitbucket.org/carnedo/hotspots_framework",
+    author="Claudia Arnedo",
+    author_email="bbglab@irbbarcelona.org",
 )
