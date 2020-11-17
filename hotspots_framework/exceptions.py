6d0652f3ecc27d2d0e7bbf19bd48e15488c95f19
I. Reyes <iker.reyes@irbbarcelona.org>
2020-11-17 10:10:00 +0100

    Removed unused error class

--- hotspots_framework/exceptions.py
+++ hotspots_framework/exceptions.py
@@ -1,23 +1,8 @@
 """
-Contains HotspotFrameworkerror class
+Contains HotspotFrameworkError class
 """
 
 
-class HotspotFrameworkerror(Exception):
+class HotspotFrameworkError(Exception):
     """Base class for exceptions in HotspotFramework"""
     pass
-
-
-class UserInputError(HotspotFrameworkerror):
-    """
-    Exception raised for errors in the input
-    """
-    def __init__(self, message):
-        """
-        Args:
-            message (str): error found
-
-        Returns:
-            None
-        """
-        self.message = message
